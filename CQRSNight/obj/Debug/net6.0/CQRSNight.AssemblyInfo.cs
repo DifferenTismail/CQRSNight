9161f180d04c81ba4a265b92b48c74476006b030
İsmail Baran <ismailbaran04@gmail.com>
2025-01-21 21:38:24 +0300

    otomatik güncellemeler işlendi

--- CQRSNight/obj/Debug/net6.0/CQRSNight.AssemblyInfo.cs
+++ CQRSNight/obj/Debug/net6.0/CQRSNight.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CQRSNight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e99e6699bf50b140adaa76116ca6ad35b3159c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CQRSNight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CQRSNight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
